bd67142a8be93047e6a6c483a59f491c1f887da2
Linda <lindanewt@gmail.com>
2026-08-13 20:13:18 -0400

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,12 +4,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title inertia>{{ config('app.name', 'ClientFlow') }}</title>
+
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.tsx'])
-    @inertiaHead
+
+    <x-inertia::head />
 </head>
 <body>
-    @inertia
+    <x-inertia::app />
 </body>
 </html>
